1118964771267dd7ecd1d004c72efca4af86b509
niklasea <mail@niklasea.dk>
2019-12-04 03:15:35 +0100

    Improved 2019 day 3 solution
    Line intersection algorithm is more readable
    Eliminated redundant loop over intersections
    Stop printing wire path inputs

--- 2019/src/bin/day3.rs
+++ 2019/src/bin/day3.rs
@@ -2,8 +2,8 @@
 // https://adventofcode.com/2019/day/3
 
 // Takes as input either
-// 1. a text file with two comma-separated wire paths on separate lines 
-// 2. two comma-separated wire paths
+// 1. a text file with two comma-separated wire paths on separate lines
+// 2. two comma-separated wire paths as arguments
 
 #[derive(Copy, Clone, Debug)]
 struct Point {
@@ -17,6 +17,8 @@ impl std::fmt::Display for Point {
 		write!(f, "(x: {}, y: {}, steps: {})", self.x, self.y, self.steps)
 	}
 }
+
+const CENTRAL_PORT: Point = Point{ x: 0, y: 0, steps: 0 };
 
 fn main() {
 	let args: Vec<String> = std::env::args().collect();
@@ -41,11 +43,10 @@ fn main() {
 		},
 	};
 
-	println!("{}", file_string);
 	let mut wires = Vec::new();
 	for line in file_string.lines() {
 		// Trim whitespace, split on commas, parse and put the result in a vector
-		let line_vectors: Vec<Point> = 
+		let line_vectors: Vec<Point> =
 			line
 			.trim()
 			.split(',')
@@ -54,32 +55,40 @@ fn main() {
 		wires.push(build_path(line_vectors));
 	}
 
-	let mut intersections = Vec::new();
-	let mut last_p: Point = Point {x: 0, y: 0, steps: 0};
+	let mut closest: Option<Point> = None;
+	let mut fewest: Option<Point> = None;
+	let mut last_p: Point = CENTRAL_PORT;
+	let mut last_q: Point = CENTRAL_PORT;
 	for p in wires[0].to_vec() {
-		let mut last_q: Point = Point {x: 0, y: 0, steps: 0};
 		for q in wires[1].to_vec() {
 			let point = find_intersection(last_p, p, last_q, q);
-			if let Some(res) = point {
-				intersections.push(res)
+			match (point, closest, fewest) {
+				(Some(p), Some(c), Some(f)) => {
+					if manhattan_distance(CENTRAL_PORT, p) < manhattan_distance(CENTRAL_PORT, c) {
+						closest = point;
+					}
+					if p.steps < f.steps {
+						fewest = point;
+					}
+				},
+				(Some(_), None, None) => {
+					closest = point;
+					fewest = point;
+				},
+				_ => (),
 			}
 			last_q = q;
 		}
 		last_p = p;
 	}
 
-	let mut closest = intersections[0];
-	let mut fewest = intersections[0];
-	for i in intersections {
-		if (i.x.abs() + i.y.abs()) < (closest.x.abs() + closest.y.abs()) {
-			closest = i;
-		}
-		if i.steps < fewest.steps {
-			fewest = i;
-		}
+	if let Some(result_one) = closest {
+		println!("Part 1 - The intersection closest to the central port is at {}, at a distance of {}", result_one, manhattan_distance(CENTRAL_PORT, result_one));
 	}
-	println!("Part 1 - The intersection closest to the central port is at {}, at a distance of {}", closest, closest.x.abs() + closest.y.abs());
-	println!("Part 2 - The intersection fewest steps from the central port is at {}", fewest);
+	if let Some(result_two) = fewest {
+		println!("Part 2 - The intersection fewest steps from the central port is at {}", result_two);
+	}
+
 }
 
 fn parse_instruction(instruction: String) -> Point {
@@ -89,49 +98,56 @@ fn parse_instruction(instruction: String) -> Point {
 		Ok(n) => n,
 	};
 	match direction {
-		"R" => Point { x: length, y: 0, steps: length},
-		"D" => Point { x: 0, y: -length, steps: length },
-		"L" => Point { x: -length, y: 0, steps: length },
-		"U" => Point { x: 0, y: length, steps: length },
+		"R" => Point{ x: length, y: 0, steps: length },
+		"D" => Point{ x: 0, y: -length, steps: length },
+		"L" => Point{ x: -length, y: 0, steps: length },
+		"U" => Point{ x: 0, y: length, steps: length },
 		d => panic!("Input file is not formatted correctly, {} is not a valid direction (instruction {})", d, instruction),
 	}
 }
 
 fn build_path(vectors: Vec<Point>) -> Vec<Point> {
-	let mut last: Point = Point {x: 0, y: 0, steps: 0};
+	let mut last: Point = Point { x: 0, y: 0, steps: 0 };
 	let mut path: Vec<Point> = Vec::new();
 	for v in vectors {
-		last = Point {x: last.x + v.x, y: last.y + v.y, steps: last.steps + v.steps};
+		last = Point{ x: last.x + v.x, y: last.y + v.y, steps: last.steps + v.steps };
 		path.push(last);
 	}
 	path
 }
 
+fn manhattan_distance(start: Point, end: Point) -> i32 {
+	(end.y - start.y).abs() + (end.x - start.x).abs()
+}
+
 // Uses the algorithm from Wikipedia
 // https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line
 fn find_intersection(p_start: Point, p_end: Point, q_start: Point, q_end: Point) -> Option<Point> {
-	let denominator = (p_start.x - p_end.x) * (q_start.y - q_end.y) - (p_start.y - p_end.y) * (q_start.x - q_end.x);
-	if denominator == 0 {
-		return None;
-	}
-	// Return if this is the central port or the denominator is 0 (parallel lines)
-	match (p_start.x, p_start.y, q_start.x, q_start.y, denominator) {
+	let Point{ x: x1, y: y1, .. } = p_start;
+	let Point{ x: x2, y: y2, .. } = p_end;
+	let Point{ x: x3, y: y3, .. } = q_start;
+	let Point{ x: x4, y: y4, .. } = q_end;
+
+	let denominator = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+	// Skip if this is the central port or the denominator is 0 (parallel lines)
+	match (x1, y1, x3, y3, denominator) {
 		(_, _, _, _, 0) => return None,
 		(0, 0, 0, 0, _) => return None,
 		_ => (),
 	}
-	let t = ((p_start.x - q_start.x) * (q_start.y - q_end.y) - (p_start.y - q_start.y) * (q_start.x - q_end.x)) as f64 / denominator as f64;
-	let u = - ((p_start.x - p_end.x) * (p_start.y - q_start.y) - (p_start.y - p_end.y) * (p_start.x - q_start.x)) as f64 / denominator as f64;
-	if 0f64 <= t && t <= 1f64 && 0f64 <= u && u <= 1f64 {
-		let intersection_x = p_start.x + (t * (p_end.x - p_start.x) as f64).round() as i32;
-		let intersection_y = p_start.y + (t * (p_end.y - p_start.y) as f64).round() as i32;
-		let p_steps = p_start.steps + (p_start.x - intersection_x).abs() + (p_start.y - intersection_y).abs();
-		let q_steps = q_start.steps + (q_start.x - intersection_x).abs() + (q_start.y - intersection_y).abs();
-		Some(Point {
-			x: intersection_x,
-			y: intersection_y,
-			steps: p_steps + q_steps,
-		})
+	let t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) as f64 / denominator as f64;
+	let u = - ((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) as f64 / denominator as f64;
+
+	if 0.0 <= t && t <= 1.0 && 0.0 <= u && u <= 1.0 {
+		let mut intersection = Point{
+			x: x1 + (t * (x2 - x1) as f64).round() as i32,
+			y: y1 + (t * (y2 - y1) as f64).round() as i32,
+			steps: 0
+		};
+		let p_steps = p_start.steps + manhattan_distance(p_start, intersection);
+		let q_steps = q_start.steps + manhattan_distance(q_start, intersection);
+		intersection.steps = p_steps + q_steps;
+		Some(intersection)
 	} else {
 		None
 	}
