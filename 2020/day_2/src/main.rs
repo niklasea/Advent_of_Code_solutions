16542344a4ede1fd9ca45be5838e2787ce15a8ee
Niklas Elmose Andersen <mail@niklasea.dk>
2020-12-06 16:02:33 +0100

    Completed 2020 day 2 part 2

--- 2020/day_2/src/main.rs
+++ 2020/day_2/src/main.rs
@@ -2,21 +2,29 @@ use regex::Regex;
 
 struct Policy {
     letter: char,
-    min: u64,
-    max: u64,
+    min: usize,
+    max: usize,
 }
 
 fn main() {
     let input = std::fs::read("./input.txt").expect("Unable to read input file.");
-    let input: String = String::from_utf8(input).expect("Input is not valid UTF-8.");
+    let input: Vec<(String, Policy)> = parse_input(String::from_utf8(input).expect("Input is not valid UTF-8."));
 
-    let valid_passwords: Vec<String> = parse_input(input)
-        .into_iter()
-        .filter(|(password, policy)| valid_password(password.to_string(), policy))
-        .map(|(password, _)| password)
+    let valid_passwords_old: Vec<String> = input
+        .iter()
+        .filter(|(password, policy)| valid_password_old(password.to_string(), policy))
+        .map(|(password, _)| password.clone())
         .collect();
     
-    println!("There are {} valid passwords in the list.", valid_passwords.len());
+    println!("There are {} valid passwords in the list according to the old policy.", valid_passwords_old.len());
+
+    let valid_passwords_new: Vec<String> = input
+    .iter()
+        .filter(|(password, policy)| valid_password_new(password.to_string(), policy))
+        .map(|(password, _)| password.clone())
+        .collect();
+    
+    println!("There are {} valid passwords in the list according to the new policy.", valid_passwords_new.len());
 }
 
 fn parse_input(input: String) -> Vec<(String, Policy)> {
@@ -26,16 +34,25 @@ fn parse_input(input: String) -> Vec<(String, Policy)> {
             (c["password"].to_string(),
             Policy {
                 letter: c["letter"].parse::<char>().unwrap(),
-                min: c["min"].parse::<u64>().unwrap(),
-                max: c["max"].parse::<u64>().unwrap(),
+                min: c["min"].parse::<usize>().unwrap(),
+                max: c["max"].parse::<usize>().unwrap(),
             }))
         .collect()
 }
 
-fn valid_password(password: String, policy: &Policy) -> bool {
-    let count: u64 = password
+fn valid_password_old(password: String, policy: &Policy) -> bool {
+    let count: usize = password
         .matches(policy.letter)
         .collect::<Vec<&str>>()
-        .len() as u64;
+        .len();
     count <= policy.max && count >= policy.min
+}
+
+fn valid_password_new(password: String, policy: &Policy) -> bool {
+    let chars: Vec<char> = password.chars().collect();
+    match (chars[policy.min - 1] == policy.letter, chars[policy.max - 1] == policy.letter) {
+        (true, false) => true,
+        (false, true) => true,
+        _ => false
+    }
 }
