c1af802153d352e7788d1cae716e83f07661eb27
Niklas Elmose Andersen <mail@niklasea.dk>
2020-12-08 01:15:24 +0100

    Completed 2020 day 5 part 2

--- 2020/day_5/src/main.rs
+++ 2020/day_5/src/main.rs
@@ -9,12 +9,20 @@ fn main() {
     let input = std::fs::read("./input.txt").expect("Unable to read input file.");
     let input = String::from_utf8(input).expect("Input file is not valid UTF-8.");
 
-    let boarding_passes = input.lines()
-        .map(|l| to_boarding_pass(l.to_string()));
+    let boarding_passes: Vec<BoardingPass> = input.lines()
+        .map(|l| to_boarding_pass(l.to_string()))
+        .collect();
 
-    let highest_seat_id = boarding_passes.map(|b| b.seat_id)
-        .max().expect("No boarding passes in input");
+    let highest_seat_id = boarding_passes.iter().max_by(|b1, b2| b1.seat_id.cmp(&b2.seat_id)).expect("No boarding passes in input").seat_id;
     println!("The highest seat ID among the boarding passes is: {}", highest_seat_id);
+
+    let my_seat_id: u64 = (0..=0b11_1111_1111).find(|i| {
+            !boarding_passes.iter().any(|b| b.seat_id == *i)
+            && boarding_passes.iter().any(|b| b.seat_id == *i + 1)
+            && boarding_passes.iter().any(|b| b.seat_id == *i - 1)
+        })
+        .unwrap();
+    println!("My seat ID is: {}", my_seat_id);
 }
 
 fn to_boarding_pass(input: String) -> BoardingPass {
