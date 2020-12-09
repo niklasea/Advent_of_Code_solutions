5a397bdcd897c1f75e66407718df75071d6222d2
Niklas Elmose Andersen <mail@niklasea.dk>
2020-12-10 00:34:10 +0100

    Completed 2020 day 6 part 2
    Wasted a lot of time because I did not know cloned() for iterators.

--- 2020/day_6/src/main.rs
+++ 2020/day_6/src/main.rs
@@ -4,13 +4,33 @@ fn main() {
     let input = std::fs::read("./input.txt").expect("Unable to read input file.");
     let input = String::from_utf8(input).expect("Input is not valid UTF-8.");
 
-    let groups: Vec<HashSet<char>> = input.split("\n\n")
+    let groups: Vec<Vec<HashSet<char>>> = input.split("\n\n")
         .map(|x| x.lines()
-            .flat_map(|l| l.chars())
+            .map(|l| l.chars().collect())
             .collect())
         .collect();
 
-    let sum_of_yes = groups.iter()
-        .fold(0, |sum, group| sum + group.len());
-    println!("The sum of \"yes\" answers from all groups on the plane is: {}", sum_of_yes);
-}
+    let union_of_answers = groups.iter()
+        .fold(0, |total, group| { 
+            total
+            + group.iter()
+                .flatten()
+                .collect::<HashSet<_>>()
+                .len()
+        });
+    println!("The union of \"yes\" answers from all groups on the plane is: {}", union_of_answers);
+
+    let start_set: HashSet<char> = "abcdefghijklmnopqrstuvwxyz".chars().collect();
+    let intersection_of_answers = groups.iter()
+        .fold(0, |total, group| 
+            total
+            + group.iter()
+                .fold(start_set.clone(), |answers, person| {
+                    answers.intersection(&person)
+                        .cloned()
+                        .collect()
+                })
+                .len()
+        );
+    println!("The intersection of \"yes\" answers from all groups on the plane is: {}", intersection_of_answers);
+}   
