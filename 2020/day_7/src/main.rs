19b4a204a02c090bfe2448b7905e7d6d34964ca4
Niklas Elmose Andersen <mail@niklasea.dk>
2020-12-13 01:26:00 +0100

    Completed 2020 day 7 part 2

--- 2020/day_7/src/main.rs
+++ 2020/day_7/src/main.rs
@@ -3,15 +3,20 @@ extern crate lazy_static;
 use regex::Regex;
 use std::collections::HashMap;
 
+struct Bag {
+	count: u64,
+	color: String,
+}
+
 fn main() {
 	let input = std::fs::read("./input.txt").expect("Unable to read input file.");
 	let input = String::from_utf8(input).expect("Input file is not valid UTF-8.");
 
-	let bags: HashMap<String, Vec<String>> = input.lines()
+	let bags: HashMap<String, Vec<Bag>> = input.lines()
 		.map(|x| bag_from_string(x.to_string()))
 		.collect();
 
-	let shiny_gold = bags.keys()
+	let shiny_gold_containers = bags.keys()
 		.filter(|x| contains_bag_of_color(
 			&bags,
 			x.to_string(),
@@ -19,32 +24,48 @@ fn main() {
 		))
 		.count();
 
-	println!("There are {} bags directly or indirectly capable of containing shiny gold bags.", shiny_gold);
+	println!("There are {} bags directly or indirectly capable of containing shiny gold bags.", shiny_gold_containers);
+
+	let my_bag = "shiny gold";
+	println!("My {} bag contains {} other bags.", my_bag, number_of_bags_contained(&bags, my_bag.to_string()));
 }
 
-fn bag_from_string(input: String) -> (String, Vec<String>) {
+fn bag_from_string(input: String) -> (String, Vec<Bag>) {
 	lazy_static! {
-		static ref RE: Regex = Regex::new(r"(\w+ \w+) bags?").unwrap();
+		static ref RE: Regex = Regex::new(r"(?P<count>\d*) ?(?P<color>\w+ \w+) bags?").unwrap();
 	}
-	let mut iter = RE.captures_iter(&input);
+	let mut bag_matches = RE.captures_iter(&input);
 	
-	(iter.next()
-		.expect("Not a valid bag: empty line.")[1]
+	(bag_matches.next()
+		.expect("Not a valid bag: empty line.")["color"]
 		.to_string(),
-	iter.map(|x| x[1].to_string())
-		.filter(|x| x != "no other")
+	bag_matches.map(|b| Bag {
+			count: b["count"].parse().unwrap_or_default(),
+			color: b["color"].to_string()
+		})
+		.filter(|bag| bag.color != "no other")
 		.collect())
 }
 
-fn contains_bag_of_color(bags: &HashMap<String, Vec<String>>, source: String, target: String) -> bool {
-	if bags[&source].contains(&target) {
+fn contains_bag_of_color(bags: &HashMap<String, Vec<Bag>>, source: String, target: String) -> bool {
+	if bags[&source].iter().any(|bag| bag.color == target) {
 		true
 	} else {
 		bags[&source].iter()
-			.any(|x| contains_bag_of_color(
+			.any(|bag| contains_bag_of_color(
 				bags, 
-				x.to_string(), 
+				bag.color.to_string(), 
 				target.to_string()
 			))
 	}
+}
+
+fn number_of_bags_contained(bags: &HashMap<String, Vec<Bag>>, source: String) -> u64 {
+	bags[&source].iter()
+		.fold(0, |sum, bag| {
+			sum + bag.count + bag.count * number_of_bags_contained(
+				&bags,
+				bag.color.to_string()
+			)
+		})
 }
